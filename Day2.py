93eb304a194ab5cb2c575ed1a2fe05814e4bfe03
Priyanka Pagare <priyankapagare71318@gmail.com>
2025-09-06 21:27:50 +0530

    dicion control code

---
+++ Day2.py
@@ -0,0 +1,16 @@
+x=int(input("Enter marks:-"))
+if x>90 and x<=100:
+    print("Grade = A")
+elif x>80 and x<=90:
+    print("Grade = B")    
+elif x>70 and x<=80:
+    print("Grade = C")
+elif x>60 and x<=70:
+    print("Grade = D")
+elif x>50 and x<=60:
+    print("Grade = E")
+else:
+    print("Grade  = F")
+    
+
+
