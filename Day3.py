3d45a95486eff701e69e644ed9de9448f245f810
Priyanka Pagare <priyankapagare71318@gmail.com>
2025-09-15 13:18:07 +0530

    Day 3

---
+++ Day3.py
@@ -0,0 +1,7 @@
+x=int(input("Enter attendation:-"))
+if x>90 and x<=100:
+    print("eligible fot MST 1 and 2")
+elif x>80 and x<=90:
+    print("eligible for MST 1 only")    
+elif x>70 and x<=80:
+    print("Not eligible")
