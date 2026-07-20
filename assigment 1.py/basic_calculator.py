210d1f919d0b47345ad27169d44edf2184774ff1
Priyanka Pagare <priyankapagare71318@gmail.com>
2026-07-20 12:16:08 +0530

    calculation

---
+++ assigment 1.py/basic_calculator.py
@@ -0,0 +1,9 @@
+# calculation in python
+a=int(input("first Number"))
+b=int(input("second Number")) 
+print("addition=",a+b)
+print("substraction=",a-b)
+print("multiplication=",a*b)
+print("division=",a/b)
+print("squre of A =", a*a)
+print("square of B=",b*b)
