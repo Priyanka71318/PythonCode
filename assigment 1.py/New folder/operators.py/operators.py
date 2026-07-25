49e7b876c6f17fcffcbb87ac334f725de0e43f5f
Priyanka Pagare <priyankapagare71318@gmail.com>
2026-07-25 21:13:49 +0530

    arthmetics operators

---
+++ assigment 1.py/New folder/operators.py/operators.py
@@ -0,0 +1,23 @@
+# Arthmatic operators
+a=int(input("enter  first no."))
+b=int(input("enter a second no."))
+ADD=a+b
+print("ADDITION TWO NUMBERS:- ", ADD)
+
+Sub=a-b
+print("substraction:-",Sub)
+
+Multiplication=a*b
+print("Multiplication:-",Multiplication)
+
+div=a/b
+print("division:- ", div)
+
+floor=a//b
+print("floor TWO NUMBERS:- ", floor)
+
+modulus=a%b
+print("modulus TWO NUMBERS:- ", modulus)
+
+Exponent=a**b
+print("exponents:- ",Exponent )
